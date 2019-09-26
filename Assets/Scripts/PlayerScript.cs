ba9d8cbfa8cdb4f1a6779266e9a0edbe491f0ecd
Eduardo Iglesias <eriglesias@ufl.edu>
2019-09-26 11:07:13 -0400

    initial commit

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    /// <summary>
+    /// The speed of the ship
+    /// </summary>
+    public Vector2 speed = new Vector2(50, 50);
+
+    // Store the movement and the component
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+
+    void Update()
+    {
+        // Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // Movement per direction
+        movement = new Vector2(
+          speed.x * inputX,
+          speed.y * inputY);
+
+    }
+
+    void FixedUpdate()
+    {
+        // Get the component and store the reference
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        // Move the game object
+        rigidbodyComponent.velocity = movement;
+    }
+}
