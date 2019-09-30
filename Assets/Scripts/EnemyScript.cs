b1786622221d64d98ec6a2b799adcfd2847a546d
Eduardo Iglesias <eriglesias@ufl.edu>
2019-09-30 16:33:51 -0400

    make level scroll and confine player to the screen

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -7,23 +7,85 @@ using UnityEngine;
 /// </summary>
 public class EnemyScript : MonoBehaviour
 {
-  private WeaponScript[] weapons;
+    private bool hasSpawn;
+    private MoveScript moveScript;
+    private WeaponScript[] weapons;
+    private Collider2D coliderComponent;
+    private SpriteRenderer rendererComponent;
 
-  void Awake()
-  {
-    // Retrieve the weapon only once
-    weapons = GetComponentsInChildren<WeaponScript>();
-  }
-
-  void Update()
-  {
-    foreach (WeaponScript weapon in weapons)
+    void Awake()
     {
-      // Auto-fire
-      if (weapon != null && weapon.CanAttack)
-      {
-        weapon.Attack(true);
-      }
+        // Retrieve the weapon only once
+        weapons = GetComponentsInChildren<WeaponScript>();
+
+        // Retrieve scripts to disable when not spawn
+        moveScript = GetComponent<MoveScript>();
+
+        coliderComponent = GetComponent<Collider2D>();
+
+        rendererComponent = GetComponent<SpriteRenderer>();
     }
-  }
+
+    // 1 - Disable everything
+    void Start()
+    {
+        hasSpawn = false;
+
+        // Disable everything
+        // -- collider
+        coliderComponent.enabled = false;
+        // -- Moving
+        moveScript.enabled = false;
+        // -- Shooting
+        foreach (WeaponScript weapon in weapons)
+        {
+            weapon.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        // 2 - Check if the enemy has spawned.
+        if (hasSpawn == false)
+        {
+            if (rendererComponent.IsVisibleFrom(Camera.main))
+            {
+                Spawn();
+            }
+        }
+        else
+        {
+            // Auto-fire
+            foreach (WeaponScript weapon in weapons)
+            {
+                if (weapon != null && weapon.enabled && weapon.CanAttack)
+                {
+                    weapon.Attack(true);
+                }
+            }
+
+            // 4 - Out of the camera ? Destroy the game object.
+            if (rendererComponent.IsVisibleFrom(Camera.main) == false)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // 3 - Activate itself.
+    private void Spawn()
+    {
+        hasSpawn = true;
+
+        // Enable everything
+        // -- Collider
+        coliderComponent.enabled = true;
+        // -- Moving
+        moveScript.enabled = true;
+        // -- Shooting
+        foreach (WeaponScript weapon in weapons)
+        {
+            weapon.enabled = true;
+        }
+    }
 }
