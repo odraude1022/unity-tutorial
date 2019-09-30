4df0bc6f54c7da6553b76bded3e4dc46dac01da2
Eduardo Iglesias <eriglesias@ufl.edu>
2019-09-29 22:49:28 -0400

    add ability for player and enemy to fire shots

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -24,8 +24,21 @@ public class PlayerScript : MonoBehaviour
           speed.x * inputX,
           speed.y * inputY);
 
-    }
+        // 5 - Shooting
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
 
+        if (shoot)
+        {
+        WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
+    }
     void FixedUpdate()
     {
         // Get the component and store the reference
@@ -34,4 +47,26 @@ public class PlayerScript : MonoBehaviour
         // Move the game object
         rigidbodyComponent.velocity = movement;
     }
+    void OnCollisionEnter2D(Collision2D collision)
+  {
+    bool damagePlayer = false;
+
+    // Collision with enemy
+    EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+    if (enemy != null)
+    {
+      // Kill the enemy
+      HealthScript enemyHealth = enemy.GetComponent<HealthScript>();
+      if (enemyHealth != null) enemyHealth.Damage(enemyHealth.hp);
+
+      damagePlayer = true;
+    }
+
+    // Damage the player
+    if (damagePlayer)
+    {
+      HealthScript playerHealth = this.GetComponent<HealthScript>();
+      if (playerHealth != null) playerHealth.Damage(1);
+    }
+  }
 }
