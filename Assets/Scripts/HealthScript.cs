b05c35d91bdb57fa6029e6cfd721291b2785235f
Eduardo Iglesias <eriglesias@ufl.edu>
2019-10-01 12:27:47 -0400

    add music and sound effects

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -26,6 +26,7 @@ public class HealthScript : MonoBehaviour
     {
       // 'Splosion!
       SpecialEffectsHelper.Instance.Explosion(transform.position);
+      SoundEffectsHelper.Instance.MakeExplosionSound();
       // Dead!
       Destroy(gameObject);
     }
